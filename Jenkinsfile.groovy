cdb6bd042f3783ac61a968d48e05e7308ba40127
Waldir Marquez <elmarqueze@gmail.com>
2024-05-14 22:02:57 -0500

    groovy file was updated

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,16 +3,18 @@ pipeline{
     stages{
         stage ('Compile Stage'){
             steps{
-                withMaven ('maven_3_9_6'){
-                    sh 'mvn clean compile'
+                def mvnHome = tool name:'maven_3_9_6', type: 'maven'
+                withMaven (["PATH+MAVEN={mvnHome}/bin"]){
+                    bat "${mvnHome}\\bin\\mvn clean compile"
                 }
             }
         }
 
         stage ('Test  Stage'){
             steps{
-                withMaven ('maven_3_9_6'){
-                    sh 'mvn clean verify -Dcucumber.filter.tags="@PRUEBA1"'
+                def mvnHome = tool name:'maven_3_9_6', type: 'maven'
+                withMaven (["PATH+MAVEN={mvnHome}/bin"]){
+                    bat "${mvnHome}\\bin\\mvn clean verify -Dcucumber.filter.tags=\"@PRUEBA1\""
                 }
             }
         }
